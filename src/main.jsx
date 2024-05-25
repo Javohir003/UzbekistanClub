cbeffa62464f626e0699c35f050d297bef40106a
Diyorbek To'raqulov <toraqulovd18@gmail.com>
2024-05-25 15:52:30 +0500

    Home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+
+import React from "react";
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './home.css'
+import  Home from  './Home/home.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Home/>
   </React.StrictMode>,
 )
