bf5ab0f6ebdf96959840f518508ff5706db3ac2a
suxrob2024 <nionz3921@gmail.com>
2024-05-27 13:35:35 +0300

    End Update

---
+++ src/Masters/Master.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Master.css'
+
+function Master() {
+  return (
+    <>
+      <div className='Slider'>
+        <div className="Inner">
+          <h1>MASTER RESIDENTS</h1>
+          <div className="nexpR">
+              <h3>All residents</h3>
+              <img className='icon-next' src="./icons/svgexport-4.svg" alt="icon" />
+          </div>
+        </div>
+        <div className="ResidentsArea">
+          <div className="item-1 item"><img src="https://uzbekclub.xn--h28h.uz/media/cache/f8/4c/f84c0ef5ed2759e6956fbfdf5fbc14d6.jpg" alt="" className="Imgs" /></div>
+          <div className="item-2 item"><img src="https://uzbekclub.xn--h28h.uz/media/cache/74/ff/74ff9577e9997a7be9d6e8b2cc4d3c06.jpg" alt="" className="Imgs" /></div>
+          <div className="item-3 item"><img src="https://uzbekclub.xn--h28h.uz/media/cache/0f/1e/0f1e839ebd0f5f00346df6ba6cb0a9d0.jpg" alt="" className="Imgs" /></div>
+          <div className="item-4 item"><img src="https://uzbekclub.xn--h28h.uz/media/cache/51/54/5154a94198a60886c97894e17ddc9f1e.jpg" alt="" className="Imgs" /></div>
+          <div className="item-5 item"><img src="https://uzbekclub.xn--h28h.uz/media/cache/e3/df/e3df6dcd2ddbc41047e13d15870e1234.jpg" alt="" className="Imgs" /></div>
+          <div className="item-6 item"><img src="https://uzbekclub.xn--h28h.uz/media/cache/27/b7/27b7542fbe2ada5f62e79b38a4fc1424.jpg" alt="" className="Imgs" /></div>
+          <div className="item-7 item"><img src="https://uzbekclub.xn--h28h.uz/media/cache/74/17/7417d3119f0b9725f789f7d0dabf8a3d.jpg" alt="" className="Imgs" /></div>
+          <div className="item-8 item"><img src="https://uzbekclub.xn--h28h.uz/media/cache/d8/40/d840477f2a8e5b87851a98451ab2a02b.jpg" alt="" className="Imgs" /></div>
+          <div className="item-9 item"><img src="https://uzbekclub.xn--h28h.uz/media/cache/9a/f3/9af3d9cca0de6c89169bcad33158d952.jpg" alt="" className="Imgs" /></div>
+          <div className="item-10 item"><img src="https://uzbekclub.xn--h28h.uz/media/cache/ac/27/ac27de8c6cdf6cb445fd0bed2d441fdb.jpg" alt="" className="Imgs" /></div>
+          <div className="item-11 item"><img src="https://uzbekclub.xn--h28h.uz/media/cache/c6/f2/c6f25daf1dd663e25029d412abc5fbe0.jpg" alt="" className="Imgs" /></div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Master
